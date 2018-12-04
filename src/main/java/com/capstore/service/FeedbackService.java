beefdfe7e6a1e0e7bf1606b75207c76d6da3ba8d
mbommath <mbommath@DIN73003010.corp.capgemini.com>
2018-12-04 17:06:57 +0530

    Team 2 Feature files added

--- src/main/java/com/capstore/service/FeedbackService.java
+++ src/main/java/com/capstore/service/FeedbackService.java
@@ -1,5 +1,20 @@
 package com.capstore.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capstore.dao.IFeedbackDao;
+import com.capstore.model.Feedback;
+
+@Service("feedbackService")
 public class FeedbackService implements IFeedbackService{
+
+	@Autowired
+	IFeedbackDao feedbackDao;
+	
+	@Override
+	public void submitFeedback(Feedback feedback) {
+		feedbackDao.save(feedback);
+	}
 
 }
---
+++ src/test/java/Manage_Inventory/StepDef.java
@@ -0,0 +1,39 @@
+package Manage_Inventory;
+
+import com.capstore.model.Inventory;
+import com.capstore.model.Product;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDef {
+
+	@Given("^Details of product$")
+	public void details_of_product() throws Throwable {
+		
+		
+	}
+
+	@When("^Edit Button is clicked$")
+	public void edit_Button_is_clicked() throws Throwable {
+	}
+
+	@Then("^Make all the fields editable$")
+	public void make_all_the_fields_editable() throws Throwable {
+	}
+
+	@Then("^Click Save Button$")
+	public void click_Save_Button() throws Throwable {
+	}
+
+	@When("^Delete Button is clicked$")
+	public void delete_Button_is_clicked() throws Throwable {
+	}
+
+	@Then("^Delete Entry$")
+	public void delete_Entry() throws Throwable {
+	}
+
+	
+}
