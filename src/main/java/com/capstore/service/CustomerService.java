5a480dead17e80fb778465d3c51066baf9c28b1f
kalsuman <kalsuman@DIN73002980.corp.capgemini.com>
2018-12-06 17:39:17 +0530

    Sanky's Update

--- src/main/java/com/capstore/service/CustomerService.java
+++ src/main/java/com/capstore/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.capstore.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,4 +35,6 @@ public class CustomerService implements ICustomerService{
 	public List<Customer> getAllCustomers() {
 		return customerDao.findAll();
 	}
+
+	
 }
