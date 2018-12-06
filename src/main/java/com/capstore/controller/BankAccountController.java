446dad2a9ee19abe14c0ea423ba33485cade865a
shreykpa <shreykpa@DIN73002976.corp.capgemini.com>
2018-12-06 14:07:29 +0530

    Transaction processing Team6

--- src/main/java/com/capstore/controller/BankAccountController.java
+++ src/main/java/com/capstore/controller/BankAccountController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,44 +16,57 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capstore.model.BankAccount;
 import com.capstore.service.IBankAccountService;
 
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/bankaccount")
 public class BankAccountController {
-	
+
 	@Autowired
 	private IBankAccountService bankAccountService;
-	
+
 	@PostMapping("/addbankaccount")
-	public ResponseEntity<String> addBankAccount(@RequestBody BankAccount bankAccount){
-		if(bankAccountService.addBankAccount(bankAccount)) {
-			return new ResponseEntity("Bank Account added successfully!",HttpStatus.OK);
-		}
-		else {
+	public ResponseEntity<String> addBankAccount(@RequestBody BankAccount bankAccount) {
+		if (bankAccountService.addBankAccount(bankAccount)) {
+			return new ResponseEntity("Bank Account added successfully!", HttpStatus.OK);
+		} else {
 			return new ResponseEntity("Sorry! Bank Account insertion failed!", HttpStatus.NOT_FOUND);
 		}
 	}
-	
+
 	@GetMapping("/getallbankaccounts")
-	public ResponseEntity<List<BankAccount>> getAllBankAccounts(){
-		List<BankAccount> bankAccounts=bankAccountService.getAllBankAccounts();
-		if(bankAccounts.isEmpty()) {
-			return new ResponseEntity("Sorry! No Saved Bank Accounts!",HttpStatus.NOT_FOUND);
+	public ResponseEntity<List<BankAccount>> getAllBankAccounts() {
+		List<BankAccount> bankAccounts = bankAccountService.getAllBankAccounts();
+		if (bankAccounts.isEmpty()) {
+			return new ResponseEntity("Sorry! No Saved Bank Accounts!", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<BankAccount>>(bankAccounts, HttpStatus.OK);
 		}
-		else {
-			return new ResponseEntity<List<BankAccount>>(bankAccounts,HttpStatus.OK);
-		}
-	}
-	
-	@PostMapping("/getbankaccfromusernameandpass")
-	public ResponseEntity<BankAccount> getBankAccountFromUserNamePassword(@RequestBody String userName, @RequestBody String userPassword){
-		List<BankAccount> bankAccounts=bankAccountService.getBankAccountFromUserNamePassword(userName, userPassword);
-		if(bankAccounts.isEmpty()) {
-			return new ResponseEntity("Sorry! No such user exists!",HttpStatus.NOT_FOUND);
-		}
-		
-		BankAccount userAccount=bankAccounts.get(0);
-		return new ResponseEntity<BankAccount>(userAccount,HttpStatus.OK);
 	}
 
+	@PostMapping("/getbankaccfromusernameandpass")
+	public ResponseEntity<BankAccount> getBankAccountFromUserNamePassword(@RequestBody String userName,
+			@RequestBody String userPassword) {
+		List<BankAccount> bankAccounts = bankAccountService.getBankAccountFromUserNamePassword(userName, userPassword);
+		if (bankAccounts.isEmpty()) {
+			return new ResponseEntity("Sorry! No such user exists!", HttpStatus.NOT_FOUND);
+		}
+
+		BankAccount userAccount = bankAccounts.get(0);
+		return new ResponseEntity<BankAccount>(userAccount, HttpStatus.OK);
+	}
+
+	@PostMapping("/depositAmount")
+	public ResponseEntity<String> depositAmount(@RequestBody BankAccount account, @RequestBody double amount) {
+		bankAccountService.depositAmount(amount, account);
+		return new ResponseEntity<String>("Amount deposited Successfully!", HttpStatus.OK);
+	}
+
+	@PostMapping("/withdrawAmount")
+	public ResponseEntity<String> withdrawAmount(@RequestBody BankAccount account, @RequestBody double amount) {
+		if (amount > account.getBalance()) {
+			return new ResponseEntity<String>("Insufficient Balance!", HttpStatus.NOT_FOUND);
+		}
+		bankAccountService.withdrawAmount(amount, account);
+		return new ResponseEntity<String>("Amount withdrawn Successfully!", HttpStatus.OK);
+	}
 }
--- src/main/java/com/capstore/service/BankAccountService.java
+++ src/main/java/com/capstore/service/BankAccountService.java
@@ -13,27 +13,35 @@ public class BankAccountService implements IBankAccountService {
 
 	@Autowired
 	private IBankAccountDao bankAccountDao;
-	
-	
+
 	@Override
 	public boolean addBankAccount(BankAccount bankAccount) {
 		bankAccountDao.save(bankAccount);
 		return true;
 	}
 
-
 	@Override
 	public List<BankAccount> getAllBankAccounts() {
-		
+
 		return bankAccountDao.findAll();
 	}
 
-
 	@Override
 	public List<BankAccount> getBankAccountFromUserNamePassword(String userName, String userPassword) {
-		
+
 		return bankAccountDao.getBankAccountFromUserNamePassword(userName, userPassword);
 	}
+	
+	@Override
+	public void depositAmount(double amount, BankAccount bankAccount) {
+		double finalAmount = bankAccount.getBalance() + amount;
+		bankAccountDao.updateAmount(finalAmount, bankAccount.getAccountNumber());
+	}
 
+	@Override
+	public void withdrawAmount(double amount, BankAccount bankAccount) {
+		double finalAmount = bankAccount.getBalance() - amount;
+		bankAccountDao.updateAmount(finalAmount, bankAccount.getAccountNumber());
+	}
 
 }
