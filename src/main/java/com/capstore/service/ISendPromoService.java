2042b115ca199093e872813649150e09b041c858
ketchauh <ketchauh@DIN73002751.corp.capgemini.com>
2018-12-08 09:02:48 +0530

    SendPromoService methods modified

--- src/main/java/com/capstore/service/ISendPromoService.java
+++ src/main/java/com/capstore/service/ISendPromoService.java
@@ -1,10 +1,14 @@
 package com.capstore.service;
 
+import java.util.List;
+
 import com.capstore.model.Email;
+import com.capstore.model.Product;
 
 public interface ISendPromoService {
 
 	public boolean sendPromotionalEmailToUser(Email email);
 	public boolean sendPromotionalEmailsToAllCustomer();
-	public Email getPromotionalEmail();
+	public Email getNewProductEmail(List<Product> products);
+	public boolean markPromotionalEmailsSent(List<Product> products);
 }
--- src/main/java/com/capstore/service/SendPromoService.java
+++ src/main/java/com/capstore/service/SendPromoService.java
@@ -30,7 +30,11 @@ public class SendPromoService implements ISendPromoService{
 	@Override
 	public boolean sendPromotionalEmailsToAllCustomer() { //Team 6
 		List<Customer> customers = customerService.getAllCustomers();
-		Email email = getPromotionalEmail();
+		
+		//get all products without email sent
+		List<Product> productsWithoutEmailSent = productService.getProductsWithoutPromotionalEmailSent();
+				
+		Email email = getNewProductEmail(productsWithoutEmailSent);
 		
 		for(Customer customer : customers) {
 			Email customerEmail = email;
@@ -38,24 +42,49 @@ public class SendPromoService implements ISendPromoService{
 			sendPromotionalEmailToUser(customerEmail);
 		}
 		
+		markPromotionalEmailsSent(productsWithoutEmailSent);
 		return true;
 	}
 
 	@Override
-	public Email getPromotionalEmail() { //Team 6
-		//get all products without email sent
-		List<Product> productsWithoutEmailSent = productService.getProductsWithoutPromotionalEmailSent();
-		
+	public Email getNewProductEmail(List<Product> products) { //Team 6
 		//make email
 		Email email = new Email();
 		email.setImageUrl("no image");
 		email.setSenderEmailId("promotion@capstore.com");
-		email.setMessage(getEmailContentFromProductList(productsWithoutEmailSent));
+		email.setMessage(getEmailContentFromProductList(products));
 		
 		return email;
 	}
-	
+
+	@Override
+	public boolean markPromotionalEmailsSent(List<Product> products) {
+		for(Product product:products) {
+			product.setPromotionMessageSent(true);
+			productService.updateProduct(product);
+		}
+		return true;
+	}
+
 	private String getEmailContentFromProductList(List<Product> products) { //Team 6
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(getHeadingForEmail());
+		stringBuilder.append("We have added following new products in our inventory. Let's have a look...!\n\n");
+		stringBuilder.append("ProductName"+"\t"+
+				"Brand"+"\t"+
+				"Discount"+"\n");
+		for(Product product:products) {
+			stringBuilder.append(product.getProductName()+"\t"+
+					product.getBrand()+"\t"+
+					product.getDiscount()+"\n");
+		}
+		stringBuilder.append("Hope you find them useful :)");
+		stringBuilder.append(getFooterForEmail());
+		
+		return stringBuilder.toString();
+	}
+	
+	private String getHeadingForEmail() {
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("\r\n" + 
 				"                                                                                                                                  \r\n" + 
@@ -71,19 +100,12 @@ public class SendPromoService implements ISendPromoService{
 				"    `8888888P'  .8'       `8. `88888. 8 8888         `Y8888P ,88P'     8 8888       `8888888P'     8 8888     `88. 8 888888888888 \r\n" + 
 				"");
 		stringBuilder.append("Greeting from CapStore\n\n");
-		stringBuilder.append("We have added following new products in our inventory. Let's have a look...!\n\n");
-		stringBuilder.append("ProductName"+"\t"+
-				"Brand"+"\t"+
-				"Discount"+"\n");
-		for(Product product:products) {
-			stringBuilder.append(product.getProductName()+"\t"+
-					product.getBrand()+"\t"+
-					product.getDiscount()+"\n");
-		}
-		stringBuilder.append("Hope you find them useful :)");
-		stringBuilder.append("Have a nice day");
-		
 		return stringBuilder.toString();
 	}
-
+	
+	private String getFooterForEmail() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Have a nice day");
+		return stringBuilder.toString();
+	}
 }
