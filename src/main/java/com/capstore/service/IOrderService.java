c232525f20500e3a3642339d0868b8611a9f6133
muasif <muasif@DIN73003011.corp.capgemini.com>
2018-12-10 15:41:03 +0530

    OrderController modified(Team 6).

--- src/main/java/com/capstore/service/IOrderService.java
+++ src/main/java/com/capstore/service/IOrderService.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import com.capstore.model.CartProduct;
 import com.capstore.model.Order;
+import com.capstore.model.Product;
 
 public interface IOrderService {
 
-	public List<CartProduct> displayCartProducts(int orderId);
+	public List<Product> displayCartProducts(int orderId);
 
 	public boolean checkAvailabilityInInventory(Order order);
 	public boolean placeOrder(Order order);
