1fabb62939dace7b21ba89851704f4e5bad09de2
gjagnani <gjagnani@DIN73002727.corp.capgemini.com>
2018-12-12 17:53:29 +0530

    Coupon Services Updated

--- src/main/java/com/capstore/dao/IWishlistDao.java
+++ src/main/java/com/capstore/dao/IWishlistDao.java
@@ -4,6 +4,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.capstore.model.Wishlist;
@@ -13,6 +14,6 @@ import com.capstore.model.Wishlist;
 public interface IWishlistDao  extends JpaRepository<Wishlist,Integer> {
 	
 	@Query("from Wishlist where customer.customerId=:customerId")
-	public Wishlist getWishlistByCustomerId(int customerId);
+	public Wishlist getWishlistByCustomerId(@Param("customerId") int customerId);
 	
 }
